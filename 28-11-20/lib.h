5900d973423d4aacc6e3597fb48c83ed91e04ee1
KokosTech <kokolia1070@gmail.com>
2020-11-27 21:54:24 +0000

    [INIT] HW-28-11-20

---
+++ 28-11-20/lib.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct people{
+
+} people;
+
+// General
+
+void init_human(){
+
+}
+
+void free_human(){
+
+}
+
+//
