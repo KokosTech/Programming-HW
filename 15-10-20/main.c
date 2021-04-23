86c0828cde1e90eac247916433378f5c700f2b10
Kaloyan Doychonov <kokolia1070@gmail.com>
2021-04-23 21:29:28 +0300

    [ADD] Stack functions from file

---
+++ 15-10-20/main.c
@@ -0,0 +1,5 @@
+#include "students.h"
+
+int main(){
+    
+}
---
+++ file_stack/stack.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FILE_NAME "stack.txt"
+
+typedef struct NNode {
+    int data;
+    struct NNode* next;
+} Node;
+
+void peek(Node* n) {
+
+    printf("Data of top Node is %d\n", n->data);
+}
+
+Node* push(Node* n, int new_val) {
+//    while (n->next) {
+//        n = n->next;
+//    }
+
+    Node* new_node = (Node*)calloc(1, sizeof(Node));
+    new_node->data = new_val;
+
+    new_node->next = n;
+//    n = &new_node;
+//    n = new_node;
+
+    return new_node;
+
+//    n->next = new_node;
+}
+
+Node* pop(Node* n) {
+//    if (n->next) {
+//        return;
+//    }
+//
+//    while (n->next->next) {
+//        n = n->next;
+//    }
+//
+//    free(n->next->next);
+//    n->next = NULL;
+
+    Node* temp_node = (Node*)calloc(1, sizeof(Node));
+    temp_node = n;
+
+    n = n->next;
+
+    free(temp_node);
+
+    return n;
+}
+
+void print(Node* n) {
+    int index = 0;
+    while(n->next) {
+        printf("Node %d = %d\n", ++index, n->data);
+        n = n->next;
+    }
+    printf("Node %d = %d\n", index, n->data);
+}
+
+Node* command_file(Node* head){
+    FILE* fp;
+    char cmd[1024];
+    int data = 0;
+
+    fp = fopen(FILE_NAME, "r");
+
+    while(1) {
+        fscanf(fp, "%s", cmd);
+
+        if(!strcmp(cmd, "PUSH")){
+            fscanf(fp, "%d", &data);
+            head = push(head, data);
+        } else if(!strcmp(cmd, "POP")){
+            head = pop(head);
+        } else if(!strcmp(cmd, "PEEK")){
+            fscanf(fp, "%d", &data);
+            peek(head);
+        } else if(!strcmp(cmd, "END")){
+            break;
+        }
+    }
+
+    fclose(fp);
+
+    return head;
+}
+
+int main() {
+    Node* head = (Node*)calloc(1, sizeof(Node));
+    head->data = 7;
+
+    if (head->next == NULL) {
+        printf("Head is initiated: data=%d\n", head->data);
+    }
+
+    printf("Before File:\n\n");
+
+    head = push(head, 9);
+    head = push(head, 11);
+    print(head);
+
+    printf("\n\nAfter File:\n\n");
+    
+    head = command_file(head);
+
+    print(head);
+
+
+    return 0;
+}
